ecb6616112999c7391d29ae4c7a8d80e6248aef4
Aryan Tayal <bookworms@ariansh.com>
2022-07-07 05:47:57 +0530

    final tweaks

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,3 +2,15 @@ const loader = document.querySelector(".loader");
 setTimeout(() => {
   loader.classList.add("hide");
 }, 3000);
+
+const form = document.querySelector("form");
+const button = document.querySelector("form button");
+const input = document.querySelector("input");
+
+form.addEventListener("submit", (e) => {
+  console.log("hi");
+  button.classList.add("disabled");
+  if (!input.value) {
+    e.preventDefault();
+  }
+});
